c00c264f229b680b4dd804f02887eabef092dce5
zangcw <chengwei.zang.1985@gmail.com>
2014-05-30 10:48:17 +0800

    configuration

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/myBlog/"
+http_images_path = "/myBlog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/myBlog/fonts"
+css_dir = "public/myBlog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
