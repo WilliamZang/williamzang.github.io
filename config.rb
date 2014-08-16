4ff5f4a3d66d3df19ac9d5bc7f8e51f778d19684
WilliamZang <chengwei.zang.1985@gmail.com>
2014-08-16 14:56:09 +0800

    修改无域名的版本

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/myBlog/"
-http_images_path = "/myBlog/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/myBlog/fonts"
-css_dir = "public/myBlog/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
